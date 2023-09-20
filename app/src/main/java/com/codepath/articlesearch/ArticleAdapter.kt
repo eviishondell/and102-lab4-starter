9250f095b54024d2aaa237f860824ed719049cb4
eviishondell <elayne297@gmail.com>
2023-09-19 21:08:40 -0400

    init

--- app/src/main/java/com/codepath/articlesearch/ArticleAdapter.kt
+++ app/src/main/java/com/codepath/articlesearch/ArticleAdapter.kt
@@ -13,8 +13,34 @@ import com.bumptech.glide.Glide
 const val ARTICLE_EXTRA = "ARTICLE_EXTRA"
 private const val TAG = "ArticleAdapter"
 
-class ArticleAdapter(private val context: Context) :
+class ArticleAdapter(private val context: Context, private val articles: List<Article>) :
     RecyclerView.Adapter<ArticleAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        private val mediaImageView: ImageView = itemView.findViewById(R.id.mediaImage)
+        private val titleTextView: TextView = itemView.findViewById(R.id.mediaTitle)
+        private val abstractTextView: TextView = itemView.findViewById(R.id.mediaAbstract)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(article: Article) {
+            titleTextView.text = article.headline?.main
+            abstractTextView.text = article.abstract
+
+            Glide.with(context)
+                .load(article.mediaImageUrl)
+                .into(mediaImageView)
+        }
+
+        override fun onClick(v: View?) {
+            val article = articles[absoluteAdapterPosition]
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(ARTICLE_EXTRA, article)
+            context.startActivity(intent)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_article, parent, false)
@@ -22,28 +48,9 @@ class ArticleAdapter(private val context: Context) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // TODO: Get the individual article and bind to holder
+        val article = articles[position]
+        holder.bind(article)
     }
 
-    override fun getItemCount() = 0
-
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
-        View.OnClickListener {
-
-        private val mediaImageView = itemView.findViewById<ImageView>(R.id.mediaImage)
-        private val titleTextView = itemView.findViewById<TextView>(R.id.mediaTitle)
-        private val abstractTextView = itemView.findViewById<TextView>(R.id.mediaAbstract)
-
-        init {
-            itemView.setOnClickListener(this)
-        }
-
-        // TODO: Write a helper method to help set up the onBindViewHolder method
-
-        override fun onClick(v: View?) {
-            // TODO: Get selected article
-
-            // TODO: Navigate to Details screen and pass selected article
-        }
-    }
+    override fun getItemCount() = articles.size
 }
